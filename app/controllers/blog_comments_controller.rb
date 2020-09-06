1fdcb72cbaaf2c7ca92fb8839e908b4b190d2033
aikosuda <aiko05.hello@gmail.com>
2020-09-06 09:55:33 +0000

    [add]controller

---
+++ app/controllers/blog_comments_controller.rb
@@ -0,0 +1,4 @@
+class BlogCommentsController < ApplicationController
+  def show
+  end
+end
---
+++ test/controllers/blog_comments_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class BlogCommentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get blog_comments_show_url
+    assert_response :success
+  end
+
+end
