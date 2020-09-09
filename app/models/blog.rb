5e55a3d054c5e8c401f1d89d3c80253c59403ab6
aikosuda <aiko05.hello@gmail.com>
2020-09-09 12:52:49 +0000

    [add]blogs/index,show,edit

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,12 +2,30 @@ class Blog < ApplicationRecord
 	belongs_to :user
 	has_many :blog_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
-	has_many :tags, through: :tag_maps
 	has_many :tag_maps, dependent: :destroy
+	has_many :tags, through: :tag_maps
 
 	attachment :image
 
 	validates :title, presence: true, length:{maximum:100}
 	validates :content, presence: true
+
+	#タグの更新処理
+	def save_tag(sent_tags)
+		current_tags = self.tags.pluck(:name) unless self.tags.nil?
+		old_tags = current_tags - sent_tags
+		new_tags = sent_tags - current_tags
+
+		#タグを消去する
+		old_tags.each do |old_tag|
+			self.tags.delete Tag.find_by(name: old_tag)
+		end
+
+		#タグを作成する
+		new_tags.each do |new_tag|
+			new_blog_tag = Tag.find_or_create_by(name: new_tag)
+			self.tags << new_blog_tag
+		end
+	end
 
 end
