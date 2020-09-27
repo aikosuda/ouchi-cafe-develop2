b6d707e948d552845305b061e059bf6803877300
aikosuda <aiko05.hello@gmail.com>
2020-09-27 10:21:38 +0000

    デプロイ用èª画像調整

---
+++ app/controllers/review_categories_controller 2.rb
@@ -0,0 +1,34 @@
+class ReviewCategoriesController < ApplicationController
+	before_action :user_admin
+
+	def create
+		@review_category = ReviewCategory.new(review_category_params)
+		if @review_category.save
+			redirect_to request.referer, notice: "レビューのカテゴリーを追加しました"
+		else
+			render 'users/show'
+		end
+	end
+
+	def destroy
+		@review_category = ReviewCategory.find(params[:id])
+		if @review_category.destroy
+			redirect_to request.referer, notice: "レビューのカテゴリーを削除しました"
+		else
+			render 'users/show'
+		end
+	end
+
+	private
+
+	def user_admin
+		if current_user.admin == false
+			redirect_to root_path
+		end
+	end
+
+	def review_category_params
+		params.require(:review_category).permit(:name)
+	end
+
+end
---
+++ app/helpers/review_categories_helper 2.rb
@@ -0,0 +1,2 @@
+module ReviewCategoriesHelper
+end
